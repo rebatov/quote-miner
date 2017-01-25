7b82374005fce39c1e49e43fb3189d563c1e8127
bishal <bishal.dangal@javra.com>
2017-01-25 21:33:44 +0545

    added CORS

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ let data = require('./technology.js')
 console.log(data[0])
 var port=Number(process.env.PORT || 3000);
 let router = express.Router();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/*',function(req,res){
   var item = data[Math.floor(Math.random()*data.length)];
   res.json({
